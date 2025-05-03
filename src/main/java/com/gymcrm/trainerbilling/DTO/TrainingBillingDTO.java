e44ec7fc9e1428593ca2aa7a2c7c0e67b9a34768
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-03 17:48:10 -0500

    refactor

--- src/main/java/com/gymcrm/trainerbilling/DTO/TrainingBillingDTO.java
+++ src/main/java/com/gymcrm/trainerbilling/DTO/TrainingBillingDTO.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TrainingBillingDTO {
+public class TrainingBillingDTO implements Serializable {
     private String trainerUsername;
     private String trainerFirstName;
     private String trainerLastName;
