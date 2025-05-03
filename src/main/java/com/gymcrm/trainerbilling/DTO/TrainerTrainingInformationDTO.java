a7d18bea83cf0a604e5a6e2526f7f1d287a918e6
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-03 15:51:30 -0500

    DockerFile and ActiveMQ 3

--- src/main/java/com/gymcrm/trainerbilling/DTO/TrainerTrainingInformationDTO.java
+++ src/main/java/com/gymcrm/trainerbilling/DTO/TrainerTrainingInformationDTO.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TrainerTrainingInformationDTO {
+public class TrainerTrainingInformationDTO implements Serializable {
     private String trainerUsername;
     private String trainerFirstName;
     private String trainerLastName;
