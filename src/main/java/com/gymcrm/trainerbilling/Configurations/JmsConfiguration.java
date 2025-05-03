3f69912418a05e6cb6c3a1fe5a937c1b98b78df7
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-03 17:11:52 -0500

    DockerFile and ActiveMQ 3

---
+++ src/main/java/com/gymcrm/trainerbilling/Configurations/JmsConfiguration.java
@@ -0,0 +1,20 @@
+package com.gymcrm.trainerbilling.Configurations;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfiguration {
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT); // expect JSON text
+        converter.setTypeIdPropertyName("_type");  // used to identify class type
+        return converter;
+    }
+}
