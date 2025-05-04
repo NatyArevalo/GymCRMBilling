a508c40d121a6c0983f9d0f9a5e944723e1255e6
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-03 20:33:27 -0500

    JmsConfiguration

--- src/main/java/com/gymcrm/trainerbilling/Configurations/JmsConfiguration.java
+++ src/main/java/com/gymcrm/trainerbilling/Configurations/JmsConfiguration.java
@@ -1,16 +1,31 @@
 package com.gymcrm.trainerbilling.Configurations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.jms.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import org.springframework.jms.support.destination.DestinationResolver;
 
 
 @Configuration
+@EnableJms
 public class JmsConfiguration {
 
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory,
+                                                                          DestinationResolver destinationResolver) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setDestinationResolver(destinationResolver);
+        factory.setSessionTransacted(true);
+        factory.setConcurrency("3-10");
+        return factory;
+    }
     @Bean
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
