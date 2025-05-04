cbca2505bdf2ee120e2fbd6df008e7dc607c85ba
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-03 20:19:31 -0500

    jacksonJmsMessageConverter

--- src/main/java/com/gymcrm/trainerbilling/Configurations/JmsConfiguration.java
+++ src/main/java/com/gymcrm/trainerbilling/Configurations/JmsConfiguration.java
@@ -1,5 +1,6 @@
 package com.gymcrm.trainerbilling.Configurations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -10,10 +11,11 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfiguration {
     @Bean
-    public MessageConverter jacksonJmsMessageConverter() {
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
         return converter;
     }
 }
