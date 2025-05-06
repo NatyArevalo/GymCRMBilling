84d518b7119cab776d0596d1c4cff53b0d5990c3
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-05 21:16:20 -0500

    Fixing Format on Training Receiver

--- src/main/java/com/gymcrm/trainerbilling/Receivers/TrainingReceiver.java
+++ src/main/java/com/gymcrm/trainerbilling/Receivers/TrainingReceiver.java
@@ -26,11 +26,10 @@ public class TrainingReceiver {
         if (trainerBilling == null) {
             logger.error("Billing failed. Invalid data.");
         } else {
-            logger.info("Billing process completed successfully.");
-            String.format("Billing process completed successfully. Trainer: %s, Month: %s, Year: %s",
+            logger.info(String.format("Billing process completed successfully. Trainer: %s, Month: %s, Year: %s",
                     trainerBilling.getTrainerUsername(),
                     trainerBilling.getYears().get(0).getYear(),
-                    trainerBilling.getYears().get(0).getMonths().get(0).getMonth());
+                    trainerBilling.getYears().get(0).getMonths().get(0).getMonth()));
 
         }
     }
